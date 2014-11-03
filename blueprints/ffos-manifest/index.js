5d8a6d204ca02456e38f8022d66b700169b954d7
Brittany Storoz <brittanystoroz@gmail.com>
2014-11-03 11:00:12 -0500

    Sync public manifest with package.json

--- blueprints/ffos-manifest/index.js
+++ blueprints/ffos-manifest/index.js
@@ -3,14 +3,5 @@ module.exports = {
     description: 'Blueprint for creating a manifest.webapp for FFOS apps',
     normalizeEntityName: function(entityName) {
       return entityName; // prevents ember-cli from requiring an entity name
-    },
-    locals: function(options) {
-        var project = this.project.pkg;
-        return {
-          appName: project.name,
-          appDescription: project.description || '',
-          authorName: project.author || '',
-          versionNumber: project.version
-        }
     }
 };
