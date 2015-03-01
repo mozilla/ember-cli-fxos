5c98c9e4e781f5c39f37c9f3141c82c7a38d7a34
Brittany Storoz <brittanystoroz@gmail.com>
2015-03-01 16:59:44 -0500

    Add fxos config file to blueprint & change blueprint name

---
+++ blueprints/ember-cli-fxos/files/config/fxos.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    consumerKey: '<your-consumer-key-here>',
+    consumerSecret: '<your-consumer-secret-here>',
+    environment: 'development'
+  },
+  production: {
+    consumerKey: '<your-consumer-key-here>',
+    consumerSecret: '<your-consumer-secret-here>',
+    environment: 'production'
+  }
+};
